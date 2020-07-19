19007a86c64e8ac9bc35c0baba0f9473c8ff92a9
PAUL A. HODGETTS <hodgetts.paul@outlook.com>
2020-07-18 20:33:41 -0400

    Commit from RStudio

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#'@importFrom tibble tibble
+NULL
+
+#' 2019 Canadian Election Online Survey.
+#'
+#' Both the online and phone survey datasets from the 2019 Canadian Election Survey (CES)
+#'
+#' @format A data frame with 620 variables and 37822 observations.
+#'
+#' For further details, see \url{https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/DUS88V}
+#' Stephenson, Laura B; Harell, Allison; Rubenson, Daniel; Loewen, Peter John, 2020, "2019 Canadian Election Study - Online Survey",
+#' \url{https://doi.org/10.7910/DVN/DUS88V}, Harvard Dataverse, V1
+"onlinesurvey"
+
+#' 2019 Canadian Election Phone Survey.
+#'
+#' @format A data frame with 273 variables and 4021 observations.
+#'
+#' For further details, see \url{https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/8RHLG1}
+#' Stephenson, Laura B; Harell, Allison; Rubenson, Daniel; Loewen, Peter John, 2020, "2019 Canadian Election Study - Phone Survey",
+#' \url{https://doi.org/10.7910/DVN/8RHLG1}, Harvard Dataverse, V1, UNF:6:eyR28qaoYlHj9qwPWZmmVQ== [fileUNF]
+"phonesurvey"
+
+#' For further details on the 2019 Canadian Election Survey, see \url{http://www.ces-eec.ca/}
