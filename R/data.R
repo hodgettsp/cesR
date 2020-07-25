a4c58ed1ec15ca2947ed3bf8af496f7bf96380a5
PAUL A. HODGETTS <hodgetts.paul@outlook.com>
2020-07-24 23:12:36 -0400

    Updated grammar in data R file

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@ NULL
 
 #' 2019 Canadian Election Online Survey.
 #'
-#' Both the online and phone survey datasets from the 2019 Canadian Election Survey (CES)
+#' The online survey dataset from the 2019 Canadian Election Survey (CES)
 #'
 #' @format A data frame with 620 variables and 37822 observations.
 #'
@@ -14,11 +14,11 @@ NULL
 
 #' 2019 Canadian Election Phone Survey.
 #'
+#' The phone survey dataset from the 2019 Canadian Election Survey (CES)
+#'
 #' @format A data frame with 273 variables and 4021 observations.
 #'
 #' For further details, see \url{https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/8RHLG1}
 #' Stephenson, Laura B; Harell, Allison; Rubenson, Daniel; Loewen, Peter John, 2020, "2019 Canadian Election Study - Phone Survey",
 #' \url{https://doi.org/10.7910/DVN/8RHLG1}, Harvard Dataverse, V1, UNF:6:eyR28qaoYlHj9qwPWZmmVQ== [fileUNF]
 "phonesurvey"
-
-#' For further details on the 2019 Canadian Election Survey, see \url{http://www.ces-eec.ca/}
