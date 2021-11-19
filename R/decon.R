ef5625738871fd0f574afc48ea60f41dcec34723
PAUL A. HODGETTS <paul.hodgetts@mail.utoronto.ca>
2021-11-19 12:30:34 -0500

    fixed doi issue
    changed url call to doi call, updated with roxygenise

--- R/decon.R
+++ R/decon.R
@@ -51,7 +51,7 @@
 #'   \url{https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/DUS88V}
 #'    Stephenson, Laura B; Harell, Allison; Rubenson, Daniel; Loewen, Peter
 #'   John, 2020, "2019 Canadian Election Study - Online Survey",
-#'   \url{https://doi.org/10.7910/DVN/DUS88V}, Harvard Dataverse, V1
+#'   \doi{10.7910/DVN/DUS88V}, Harvard Dataverse, V1
 
 
 #library(dplyr)
